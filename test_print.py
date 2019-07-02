91c41c4ae1a89b5cd3bd3bd9f5fa57ad5297e4ed
Marius Feteanu <marius.feteanu@gmail.com>
2019-07-02 13:22:29 +0100

    Fix test print

--- test_print.py
+++ test_print.py
@@ -1,31 +1,28 @@
 import faust
 
-app = faust.App(
-    'whatevs-app',
-    broker='kafka://kafka1:9092',
-    value_serializer='raw',
-)
+app = faust.App('test-example')
 
 test_topic = app.topic('test_topic')
 
-print('Starting')
 
 @app.agent(test_topic)
 async def greet(messages):
     async for message in messages:
         print(message)
-
-print('Going out the end')
+        yield message
 
 
 async def test_greet():
     # start and stop the agent in this block
     async with greet.test_context() as agent:
         await agent.put("Hello there")
+        await agent.put("General Kenobi.")
+
 
 async def run_tests():
     app.conf.store = 'memory://'   # tables must be in-memory
     await test_greet()
+
 
 if __name__ == '__main__':
     import asyncio
