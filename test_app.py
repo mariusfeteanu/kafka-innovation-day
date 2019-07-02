4b70e2117700586d39320acdde35242e679a39d4
Marius Feteanu <marius.feteanu@gmail.com>
2019-07-02 15:52:36 +0100

    scare awawy warning

--- test_app.py
+++ test_app.py
@@ -8,7 +8,8 @@ from app import *
 def test_app(event_loop):
     """passing in event_loop helps avoid 'attached to a different loop' error"""
     app.finalize()
-    app.conf.store = 'memory://'
+    if not 'memory' in str(app.conf.store):
+        app.conf.store = 'memory://'
     app.flow_control.resume()
     return app
 
