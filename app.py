fdb36c23296174bc0ef5786bb72d723e8bccedb8
Marius Feteanu <marius.feteanu@gmail.com>
2019-07-02 15:50:33 +0100

    We have tables

--- app.py
+++ app.py
@@ -13,10 +13,12 @@ class EnquiryInitiated(faust.Record, serializer='json'):
     enquiry_id: str
     date: str
     product: str
+    account_id: str
 
 class EnquiryCompleted(faust.Record, serializer='json'):
     enquiry_id: str
     date: str
+    best_partner: str
 
 class SaleCompleted(faust.Record, serializer='json'):
     sale_id: str
@@ -24,10 +26,18 @@ class SaleCompleted(faust.Record, serializer='json'):
     date: str
     amount: float
 
+class FullEnquiryCompleted(faust.Record, serializer='json'):
+    enquiry_id: str
+    completed_date: str
+    product: str
+    best_partner: str
+
 registered_topic = app.topic('registered_topic')
 enquiry_initiated_topic = app.topic('enquiry_initiated_topic')
 enquiry_completed_topic = app.topic('enquiry_completed_topic')
 sale_completed_topic = app.topic('sale_completed_topic')
+
+enquiry_initiated_table = app.Table('(product, date)', default=int)
 
 
 def mask_user(user):
@@ -42,3 +52,25 @@ async def mask_pii(registered_users):
     async for registered_user in registered_users:
         masked_user = mask_user(registered_user)
         yield masked_user
+
+@app.agent(enquiry_initiated_topic)
+async def calculate_enquiries_per_date(enquiries_initiated):
+    async for enquiry_initiated in enquiries_initiated:
+        enquiry_initiated_table[(enquiry_initiated.product, enquiry_initiated.date)] += 1
+        yield enquiry_initiated
+
+# @app.task()
+# async def full_enquiry_completed_stream():
+#     enquiry_initiated = enquiry_initiated_topic.stream()
+#     enquiry_completed = enquiry_completed_topic.stream()
+#     print("STARTED: full_enquiry_completed_stream")
+#     async for event in (enquiry_initiated & enquiry_completed).join():
+#         print("EVENT: ", event)
+#         yield event
+#     print("STOP: Falling out of task")
+
+#
+# @app.agent()
+# async def somethingsomething(full_enquiry_completed_stream):
+#     async for event in full_enquiry_completed_stream:
+#         yield event
--- test_app.py
+++ test_app.py
@@ -1,5 +1,5 @@
-
 import pytest
+import asyncio
 
 from app import *
 
@@ -12,21 +12,68 @@ def test_app(event_loop):
     app.flow_control.resume()
     return app
 
-
 @pytest.mark.asyncio()
-async def test_pii(test_app):
-    async with mask_pii.test_context() as agent:
-        marius = RegisteredUser(account_id='kjduebvfds',
-                                email='marius.something@gmail.cob',
-                                name='Marius')
-        await agent.put(marius)
-7
-        print(marius.to_representation())
+async def test_calculate_enquiries_per_date(test_app):
+    async with calculate_enquiries_per_date.test_context() as agent:
+        eq1 = EnquiryInitiated(enquiry_id='1',
+                               date='2019-01-01',
+                               product='A',
+                               account_id='B')
+        eq2 = EnquiryInitiated(enquiry_id='1',
+                               date='2019-01-02',
+                               product='A',
+                               account_id='B')
+        eq3 = EnquiryInitiated(enquiry_id='1',
+                               date='2019-01-02',
+                               product='B',
+                               account_id='B')
+        eq4 = EnquiryInitiated(enquiry_id='1',
+                               date='2019-01-03',
+                               product='B',
+                               account_id='B')
+        eq5 = EnquiryInitiated(enquiry_id='1',
+                               date='2019-01-03',
+                               product='B',
+                               account_id='B')
+        await agent.put(eq1)
+        await agent.put(eq2)
+        await agent.put(eq3)
+        await agent.put(eq4)
+        await agent.put(eq5)
 
-        assert len(agent.results) == 1
-        
-        masked_marius = agent.results[0]
+    assert enquiry_initiated_table[('A', '2019-01-01')] == 1
+    assert enquiry_initiated_table[('A', '2019-01-01')] == 1
+    assert enquiry_initiated_table[('B', '2019-01-02')] == 1
+    assert enquiry_initiated_table[('A', '2019-01-01')] == 1
+    assert enquiry_initiated_table[('B', '2019-01-03')] == 2
 
-        assert masked_marius.account_id == 'kjduebvfds'
-        assert masked_marius.email =='boc.liamg@gnihtemos.suiram'
-        assert masked_marius.name == 'suiraM'
+# @pytest.mark.asyncio()
+# async def test_pii(test_app):
+#     async with mask_pii.test_context() as agent:
+#         marius = RegisteredUser(account_id='kjduebvfds',
+#                                 email='marius.something@gmail.cob',
+#                                 name='Marius')
+#         await agent.put(marius)
+#
+#         print(marius.to_representation())
+#
+#         assert len(agent.results) == 1
+#
+#         masked_marius = agent.results[0]
+#
+#         assert masked_marius.account_id == 'kjduebvfds'
+#         assert masked_marius.email =='boc.liamg@gnihtemos.suiram'
+#         assert masked_marius.name == 'suiraM'
+
+# @pytest.mark.asyncio()
+# async def test_full_enquiry_completed(test_app):
+#     async with full_enquiry_completed.test_context() as agent:
+#         # put some initiated enquiries
+#         # put some completed enquiries
+#         ei1 = EnquiryInitiated(enquiry_id='kjduebvfds',
+#                                 date='2018-01-01',
+#                                 product='insurances',
+#                                 account_id='kjduebvfds')
+#         await agent.put(ei1)
+#
+#         assert len(agent.results) == 1
